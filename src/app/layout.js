84281147693a11463870125a09c023c257f1dff6
diogocerutti <diogo.cerutti19@gmail.com>
2026-09-24 10:51:58 -0300

    Ajustes no Header

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Header from "../components/Header.js";
 
 export const metadata = {
   title: "Create Next App",
@@ -8,6 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Header />
       <body>{children}</body>
     </html>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,9 @@
 import styles from "./page.module.css";
-import logo from "../../public/img/logo.png";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <header className={styles.header}>
-        <img src={logo.src} className={styles.logo} />
-        <a>MENU</a>
-        <a>SOBRE NÓS</a>
-        <a>CONTATO</a>
-        <a>ORÇAMENTO</a>
-      </header>
-      <main className={styles.main}></main>
+      <main className={styles.main}>PÁGINA PRINCIPAL</main>
     </div>
   );
 }
